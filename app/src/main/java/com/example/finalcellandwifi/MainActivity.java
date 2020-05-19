7b165da1644c55978f7c3726c49ff7d476ea1c12
nirnimeshghose <nghose@unl.edu>
2020-05-19 12:30:00 -0500

    New

--- app/src/main/java/com/example/finalcellandwifi/MainActivity.java
+++ app/src/main/java/com/example/finalcellandwifi/MainActivity.java
@@ -11,7 +11,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.icu.util.Calendar;
-import android.location.Criteria;
 import android.location.Location;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
